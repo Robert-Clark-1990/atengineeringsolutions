d3f55e2818c010c6b0f95eea459c8123c6ddb1a2
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-06-23 11:13:03 +0000

    add functioning slick slider set to autoplay reviews

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,13 @@
 
 $(document).ready(function(){
-    $('.carousel-items').slick({
+    $('#reviews .carousel-items').slick({
         infinite: true,
-        dots: true,
         slidesToShow: 2,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         responsive: [
-            {
+            { 
               breakpoint: 480,
               settings: {
                 slidesToShow: 1,
