24e444c77b0ae2888c7522b2cf390c5d9cf06fe7
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-06-28 11:27:38 +0000

    start work on contact form to work correctly

--- atengineeringsolutions/settings.py
+++ atengineeringsolutions/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6c^ypeg=1&fdp@m9bv#je30%@n^xrg*y2pe80bc!mq358an343'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -156,3 +159,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
--- home/views.py
+++ home/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 from .models import Reviews
-from .forms import ReviewsForm
 
 
 def index(request):
 
     reviews = Reviews.objects.all()
 
-    context = {
-        'reviews': reviews,
-    }
+    if request.method == 'POST':
+        name = request.POST['name']
+        number = request.POST['number']
+        email = request.POST['email']
+        message = request.POST['message']
 
-    return render(request, 'home/index.html', context)
+        # send an email
+        send_mail(
+            'Request from' + name,  # subject
+            'Sender:' + name + '\n Tel:' + number + '\n Message:' + message,
+            email,  # from email
+            ['atengineeringsolutions@gmail.com']  # to email
+        )
+
+        context = {
+            'reviews': reviews,
+            'name': name,
+            'number': number,
+            'email': email,
+            'message': message,
+        }
+        return render(request, 'home/index.html', context)
+    else:
+        context = {
+            'reviews': reviews,
+        }
+        return render(request, 'home/index.html', context)
