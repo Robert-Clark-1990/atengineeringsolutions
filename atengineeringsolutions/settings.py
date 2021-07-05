59d818f30287c5624f05e230fe072d6ef59de8d6
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-07-05 13:33:39 +0000

    update colour scheme per client request

--- atengineeringsolutions/settings.py
+++ atengineeringsolutions/settings.py
@@ -93,7 +93,8 @@ SITE_ID = 1
 
 # Email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'atengineeringsolutions@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
